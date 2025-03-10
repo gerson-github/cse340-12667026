56f904aa7ef4576d0c1cd768792bae97c4d3c58d
gerson <gersonmoliver@gmail.com>
2025-03-10 12:25:23 -0700

    initial commit

--- server.js
+++ server.js
@@ -9,17 +9,34 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+//index route
+app.get('/', (req, res) => {
+  console.log('user hit the resource')
+  res.render("index", {title:"home"})
+  //res.status(200).send('Home Page')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('Resource not fount !')
+})
+
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 const host = process.env.HOST
 
 /* ***********************
