57d4e317fa1afe48d0bf903d6474a2dce16f711f
gerson <gersonmoliver@gmail.com>
2025-05-09 20:00:06 -0700

    my view project saved

--- myvueapp/js/main.js
+++ myvueapp/js/main.js
@@ -1,1 +1,52 @@
-var product = 'Socks'
+//var product = 'Socks'
+const app = Vue.createApp({
+    data() {
+      return {
+        product: 'Socks',
+        image: './images/socks-image.jpg',
+        pijama: './images/pijama.jpg',
+        likes: 0,
+        inStock: true,
+        inventory: 11,
+        details: ["Gerson", "Maria", "Joao"],
+        variants:[
+          {
+            variantId: 2234,
+            variantColor: "green",
+            variantImage: './images/socks-image.jpg'
+          },
+          {
+            variantId: 1122,
+            variantColor: "yellow",
+            variantImage: './images/bug.jpg'
+          }
+        ],
+        cart:0
+      }
+      
+    },
+    methods: {
+      changeProduct() 
+      {
+        this.product = 'cars'
+      },
+      likeProduct() 
+      {
+        this.likes++
+      },
+      addToCart: function () 
+      {
+        this.cart +=1
+      },
+      updateProduct: function (variantImage)
+      {
+        this.image = variantImage
+      }
+
+
+    }
+
+  })
+  
+  app.mount('#app')
+  
